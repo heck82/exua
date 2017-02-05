f151cc29aa1f2d045243ae80401d6b9d705c6fa2
heck82 <andriy.khekalo@edvantis.com>
2017-02-05 18:26:01 +0200

    test commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ mongoose.connect('mongodb://localhost/exua');
 var db = mongoose.connection;
 mongoose.Promise = global.Promise;
 
-//								MODELS
+//								MODELS (changes....)
 
 var Item = require('./models/item');
 var User = require('./models/user');
