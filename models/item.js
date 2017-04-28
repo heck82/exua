b8aeee2e5811b132caf77e464ba1b2d14dec8c4a
heck82 <heck82@meta.ua>
2017-04-28 22:00:55 +0300

    for holidays

--- models/item.js
+++ models/item.js
@@ -1,17 +1,18 @@
 var mongoose = require('mongoose');
 
 var itemSchema = new mongoose.Schema({
-	available: Boolean,
-	public: Boolean,
-	own: Boolean,
-	name: String,
-	category: String,
-	tags: [String],
-	description: String,
-	coverImageUrl: String,
-	size: Number,
-	createDate: Date,
-	uptoDate: Date
+    available: Boolean,
+    public: Boolean,
+    own: Boolean,
+    name: String,
+    nameLoCase: String,
+    category: String,
+    tags: [String],
+    description: String,
+    coverImageUrl: String,
+    size: Number,
+    createDate: Date,
+    uptoDate: Date
 });
 
 module.exports = mongoose.model('Items', itemSchema);
